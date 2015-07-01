322f91de584311bc9dbacda1100a5a8567335c50
Owtopsy <donia_b_isn@hotmail.fr>
2015-07-01 23:56:36 +0200

    projet unity avec son binaural et gant

---
+++ Unity/son binaural/Assets/Scripts/vhtIOConn.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System;
+
+public class vhtIOConn : MonoBehaviour {
+
+	[SerializeField]
+	Transform palm;
+
+	[SerializeField]
+	Transform thumb;
+	[SerializeField]
+	Transform thumb1;
+	
+	[SerializeField]
+	Transform index;
+	[SerializeField]
+	Transform index1;
+	
+	[SerializeField]
+	Transform major;
+	[SerializeField]
+	Transform major1;
+	
+	[SerializeField]
+	Transform ring;
+	[SerializeField]
+	Transform ring1;
+	
+	[SerializeField]
+	Transform pinky;
+	[SerializeField]
+	Transform pinky1;
+	
+	[DllImport("CyberGloveForUnity", EntryPoint="getCyberTouch", CallingConvention=CallingConvention.StdCall)]
+	static public extern IntPtr getCyberTouch();
+
+	[DllImport("CyberGloveForUnity", EntryPoint="setVibration", CallingConvention=CallingConvention.StdCall)]
+	static public extern void setVibration (IntPtr touch, int finger, double value);
+
+	[DllImport("CyberGloveForUnity", EntryPoint="getData", CallingConvention=CallingConvention.StdCall)]
+	static public extern double getData (IntPtr touch, int finger, int joint);
+
+	[DllImport("CyberGloveForUnity", EntryPoint="updateGloveData", CallingConvention=CallingConvention.StdCall)]
+	static public extern void updateGloveData (IntPtr touch);
+	// Use this for initialization
+
+	IntPtr glove;
+
+	double[,] gloveData;
+
+	void Start () {
+
+		gloveData = new double[6,3];
+		glove = getCyberTouch();
+		setVibration (glove, 0, 0.0);
+
+	}
+
+	public void vibrate(int finger, double value)
+	{
+		setVibration (glove, finger, value);
+	}
+
+	public double [,] getGloveData()
+	{
+		return gloveData;
+	}
+
+	public void updateGlove()
+	{
+		for (int finger = 0; finger < 6; finger++)
+			for (int joint = 0; joint < 3; joint++)
+				gloveData [finger,joint] = getData (glove, finger, joint);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		updateGloveData (glove);
+		updateGlove ();
+		//thumb.localRotation = Quaternion.Euler (0, 0, (float)(-(gloveData [0, 0]*180)/Math.PI));
+		//thumb1.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [0, 1]*180)/Math.PI));
+		index.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [1, 0]*180)/Math.PI));
+		index1.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [1, 1]*180)/Math.PI));
+		major.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [2, 0]*180)/Math.PI));
+		major1.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [2, 1]*180)/Math.PI));
+		ring.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [3, 0]*180)/Math.PI));
+		ring1.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [3, 1]*180)/Math.PI));
+		pinky.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [4, 0]*180)/Math.PI));
+		pinky1.localRotation = Quaternion.Euler (0, 0, (float)((gloveData [4, 1]*180)/Math.PI));
+	}
+}
---
+++ Unity/son binaural/Assets/TBE_3Dception/Scripts/PlayAudioSources.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudioSources : MonoBehaviour {
+	
+	public AudioClip[] dialogue;
+	private GameObject dummyAudioObject;
+	private Vector3 newPosition;
+	private int positionCounter;
+	
+	void Start () {
+		dummyAudioObject = new GameObject ();
+		dummyAudioObject.AddComponent<AudioSource> ();
+		//dummyAudioObject.AddComponent<ReadAmplitude> ();
+		dummyAudioObject.GetComponent<AudioSource>().bypassReverbZones = true;
+		dummyAudioObject.GetComponent<AudioSource>().spatialBlend = 0;
+
+		newPosition = transform.position;
+
+		InvokeRepeating ("playRandomDia", 1, 7);
+		InvokeRepeating ("moveRobot", 1, 10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, newPosition, Time.deltaTime*0.5f);
+	}
+
+	void playRandomDia() {
+		int i = Random.Range (0, 10);
+		gameObject.GetComponent<TBE_3DCore.TBE_Source> ().PlayOneShot (dialogue [i]);
+		dummyAudioObject.GetComponent<AudioSource>().PlayOneShot (dialogue[i]);
+	}
+
+	void moveRobot() {
+
+		positionCounter++;
+		float randomAddition = Random.Range (0.01f, 0.09f);
+		switch (positionCounter) {
+		case 1:
+			newPosition = new Vector3(-2.32f, 4f, -14.387f);
+			break;
+		case 2:
+			newPosition = new Vector3(1.704f, 1.78f, -14.387f);
+			break;
+		case 3:
+			newPosition = new Vector3(3.765f, 0.634f, -14.38f);
+			break;
+		case 4:
+			newPosition = new Vector3(-0.7374f, 1.7f, -14.647f);
+			break;
+		case 5:
+			newPosition = new Vector3(-0.252f+randomAddition, 1.692f, -10.74f);
+			break;
+		}
+		if (positionCounter == 4)
+			positionCounter = 0;
+	}
+}
