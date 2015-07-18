0f82457f6dc1d6635a5d095994e20d62895fd7df
Tecnop^ <mr_p_75@hotmail.fr>
2015-07-18 17:26:22 +0200

    Kinect

---
+++ Unity/son binaural/Assets/Scripts/Entities/EntityManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityManager : MonoBehaviour {
+
+	[SerializeField]
+	Transform _transform;
+
+	[SerializeField]
+	Stats _stats;
+
+
+
+	// Static
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void moveTo(Vector3 dest){
+		_transform.position = dest;
+
+	}
+
+	public void rotateTo(Quaternion rot){
+
+		//_transform.rotation = rot;
+	}
+
+	// GET / SET
+
+	public Transform objectTransform {
+		get {
+			return _transform;
+		}
+	}
+
+	public Stats stats {
+		get {
+			return _stats;
+		}
+	}
+}
---
+++ Unity/son binaural/Assets/Scripts/Player/ColliderRotationScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ColliderRotationScript : MonoBehaviour {
+
+	[SerializeField]
+	GameObject _object;
+
+	private bool _active;
+	private Action<GameObject> _stayAction;
+	private Action<GameObject> _enterAction;
+	private Action<GameObject> _exitAction;
+
+	public void activate (Action<GameObject> onStay,Action<GameObject> onEnter,Action<GameObject> onExit){
+		_stayAction = onStay;
+		_enterAction = onEnter;
+		_exitAction = onStay;
+
+		_active = true;
+		//_object.SetActive(true);
+
+	}
+
+	// Use this for initialization
+	void Awake () {
+		_active = false;
+		//_object.SetActive(false);
+	}
+	void OnTriggerEnter (Collider col){
+		if (!_active)
+			return;
+
+		_enterAction (col.gameObject);
+	}
+
+	void OnTriggerExit (Collider col){
+		if (!_active)
+			return;
+		
+		_exitAction (col.gameObject);
+	}
+
+	void OnTriggerStay (Collider col){
+		if(!_active)
+			return;
+
+
+		_stayAction(col.gameObject);
+	}
+
+	public bool active {
+		get {
+			return _active;
+		}
+		set {
+			_active = value;
+		}
+	}
+}
