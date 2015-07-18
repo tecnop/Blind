594858a999d9ae679088a52065590cfa72cf964b
Owtopsy <donia_b_isn@hotmail.fr>
2015-07-18 13:52:53 +0200

    Adding sub proj

---
+++ Unity/son binaural/Assets/Scripts/SwitchAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SwitchAudio : MonoBehaviour {
+	
+	public AudioClip[] myAudioClip;
+    [SerializeField]
+    private AudioSource audio;
+	private int currentClip = 0;
+ 
+	void Start () {
+	    //LoopClips();
+        StartCoroutine("LoopClips");
+	}
+ 
+	IEnumerator LoopClips () {
+        while (true) {
+            audio.clip = myAudioClip[currentClip];
+            audio.Play();
+            yield return new WaitForSeconds(audio.clip.length);
+
+            currentClip++;
+
+            if (currentClip >= myAudioClip.Length)
+                currentClip = 0;
+        }
+    }
+}
---
+++ Unity/son binaural/Assets/Scripts/SwitchSource.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using TBE_3DCore;
+
+
+public class SwitchSource : MonoBehaviour {
+	
+	public AudioClip[] myAudioClip;
+    [SerializeField]
+    TBE_Source tbeSource;
+    [SerializeField]
+    TBE_Source tbeSource2;
+	private int currentClip = 0;
+ 
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            tbeSource.Play();
+            if (tbeSource2)
+            {
+                StartCoroutine("LoopClips");
+            }
+        }
+    }
+
+	IEnumerator LoopClips () {
+        while (true) {
+
+            yield return new WaitForSeconds(tbeSource.clip.length + 2);
+            tbeSource2.clip = myAudioClip[currentClip];
+            tbeSource2.Play();
+            yield return new WaitForSeconds(tbeSource2.clip.length + 2);
+
+            currentClip++;
+
+            if (currentClip >= myAudioClip.Length)
+                currentClip = 0;
+        }
+    }
+}
